6b5efb42b182dc1ce6fbf614cf2e9a13ceef7f7d
Anton Honcharov <4eyalush@gmail.com>
2015-11-18 18:30:15 +0200

    adding normalize.css and some regulation

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
-import HelloWorld from './component.jsx';
+import HelloWorld from './components/hello_component.jsx';
 import React from 'react';
 import ReactDom from 'react-dom';
 require('./app.scss');
-console.log('I am root file')
+
+console.log('app.jsx')
 ReactDom.render(<HelloWorld />, document.getElementById('main'));
