28cdf10a2d2f532416bde7f981262c549931d002
Anton Honcharov <4eyalush@gmail.com>
2015-11-22 13:44:13 +0200

    js fix in webpack config and slider fix

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: "app.jsx",
-    publicPath: '/static/'
+    filename: "app.js"
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
